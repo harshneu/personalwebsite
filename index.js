8dcbe6f1d22a04f4cd6f3bbadf8eb554e8d8e0a8
harshneu <hrsh.shuklaa@gmail.com>
2017-08-02 00:33:00 -0400

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
