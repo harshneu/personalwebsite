c9d4516af565eb8e826803c297751a7d7f33016e
harshneu <hrsh.shuklaa@gmail.com>
2017-08-02 00:59:01 -0400

    Asd

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.redirect('pages/index');
 });
 
 app.listen(app.get('port'), function() {
