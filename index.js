6ed0f0b02ea6554ddb4b2a4a0fe7490aa2f38815
harshneu <hrsh.shuklaa@gmail.com>
2017-08-02 00:48:11 -0400

    Asd

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  res.send('index.html');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
